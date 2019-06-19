af7cedb387cca105f12ab7cc2c3b9bba1037c3d8
huandrio <49045161+huandrio@users.noreply.github.com>
2019-06-18 21:58:35 -0300

    adicionando projeto medicamento

---
+++ Projeto_Medicamento/Projeto_Medicamento/Lote.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Medicamento
+{
+    class Lote
+    {
+        int id;
+        int qtde;
+        DateTime venc;
+
+        public int Id { get { return this.id; } set { id = value; } }
+        public int Qtde { get { return this.qtde; }set { qtde = value; } }
+        public DateTime Venc { get { return this.venc; } set { venc = value; } }
+
+        public Lote() {
+
+        }
+
+        public Lote(int id, int qtde, DateTime venc) {
+            this.id = id;
+            this.qtde = qtde;
+            this.venc = venc;
+        }
+
+        public string toString() {
+            // retornar id + “-“ + qtde + “-“ + venc
+            string str = " - Lote Id: " + Id + " - Quantidade: " + Qtde + " - Vencimento em: " + Venc;
+            return str;
+        }
+        public override bool Equals(object obj)
+        {
+            Lote l = ((Lote)obj);
+            return this.id.Equals(l.Id);
+        }
+    }
+}
---
+++ Projeto_Medicamento/Projeto_Medicamento/Medicamentos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Medicamento
+{
+    class Medicamentos
+    {
+        List<Medicamento> listaMedicamentos;
+
+        public List<Medicamento> ListaMedicamentos { get{ return listaMedicamentos; } set{ this.listaMedicamentos = value; }}
+
+        public Medicamentos()
+        {
+            listaMedicamentos = new List<Medicamento>();
+        }
+
+        public void adicionar(Medicamento medicamento) {
+            if (pesquisar(medicamento)== null)
+                listaMedicamentos.Add(medicamento);
+        }
+
+        public bool deletar(Medicamento medicamento){
+            medicamento = pesquisar(medicamento);
+            
+            if (medicamento.Lotes.Count == 0 || medicamento.qtdeDisponivel() == 0)
+            {
+                listaMedicamentos.Remove(medicamento);
+                return true;
+            }
+            else return false;
+        }
+
+        public Medicamento pesquisar(Medicamento medicamento) {
+           foreach (Medicamento m in listaMedicamentos)
+            {
+                if (m.Equals(medicamento))
+                    return m;
+            }
+            return null;
+        }
+    }
+}
